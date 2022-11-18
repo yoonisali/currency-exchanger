0d2f1bc1cc04a174b11162f07e353452b4678c89
yoonisalii <yoonismahmoudali@gmail.com>
2022-11-18 09:12:04 -0800

    add currencyAPI.js file which will fetch the API

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,6 +16,7 @@ module.exports = {
   devtool: "eval-source-map",
   plugins: [
     new ESLintPlugin(),
+    new Dotenv(),
     new CleanWebpackPlugin({
       verbose: true,
     }),

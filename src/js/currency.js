0618777ab08aa7ffa4313c7c3ba1882b5d6e525c
yoonisalii <yoonismahmoudali@gmail.com>
2022-11-18 11:45:53 -0800

    WIP to display results

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,35 +1,40 @@
-import './css/styles.css';
-import CurrencyExchangerService from './currencyAPI.js';
+import "./css/styles.css";
+import CurrencyExchangerService from "./currencyAPI.js";
 
-// Business Logic 
-async function getRates() {
-  const response = await  CurrencyExchangerService.getRates();
-  if (response) {
-    displayExchange();
+// Business Logic
+async function getRates(currencies, dollar) {
+  const response = await CurrencyExchangerService.getRates(currencies, dollar);
+  if (response.result === "success") {
+    printElements(response, currencies, dollar);
+  } else {
+    printError(response, currencies, dollar);
   }
-
 }
 
 // UI Logic -------------------------------------------------------------------------------------------------------------------
 
-const displayExchange = (response, usDollar, currency) => {
+const printElements = (response, dollar, currencies) => {
+    document.getElementById( "displayExchange" ).innerText = `${dollar} in ${currencies}: ${response.conversion_rates[currencies] * dollar}`;
+};
+
+const printError = (response, dollar, currencies) => {
   if (response.result !== "success") {
-    document.getElementById("displayExchange").innerText = `There was an error: ${response.message}`;
-  } else if (!currency) {
-    document.getElementById("displayExchange").innerText = "Please select a currency."
-  } else if (usDollar === '') {
-    document.getElementById("displayExchange").innerText = "Please enter an amount in USD."
-  } else if (response.result === "success") {
-    document.getElementById("displayExchange").innerText = `${usDollar} in ${currency}: ${response.conversion_rates[currency]*usDollar}`;
-  }
-}
+    document.getElementById("displayExchange").innerText = `There was an error: ${response[error-type]}`;
+  } else if (!currencies) {
+    document.getElementById("displayExchange").innerText = "Please select a currency.";
+  } else if (dollar === "") {
+    document.getElementById("displayExchange").innerText = "Please enter an amount in USD.";
+  } 
+};
+
 const handleSubmit = (event) => {
   event.preventDefault();
   const userCurrency = document.getElementById("chooseCurrency").value;
   const userAmount = document.getElementById("chooseAmount").value;
   getRates(userCurrency, userAmount);
-}
+}; 
+
 
 window.addEventListener("load", function () {
   document.getElementById("currencyExchanger").addEventListener("submit", handleSubmit);
-})
+});
--- src/js/currencyAPI.js
+++ src/js/currencyAPI.js
@@ -1,9 +1,9 @@
 export default class CurrencyExchangerService {
-  static async getRates() {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+  static async getRates(dollar, currencies) {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencies}/${dollar}`); 
     const jsonifiedResponse = await response.json();
     if (!response.ok) {
-      const errorMessage = `${response.status} ${respone.statusText} ${jsonifiedResponse.message}`;
+      const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse[error-type]}`;
       throw new Error(errorMessage);
     }
     return jsonifiedResponse;

a83419445e1518402ec4902a0378742a79e0855a
yoonisalii <yoonismahmoudali@gmail.com>
2022-11-18 12:48:30 -0800

    add UI logic to display results

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import './css/styles.css';
+import CurrencyExchangerService from "./js/currencyAPI.js"
+
+// Business Logic
+async function getRates(currency) {
+  const response = await CurrencyExchangerService.getRates(currency);
+  if (response.result === "success") {
+    printElements(response, currency);
+  } else {
+    printError(response, currency);
+  }
+}
+
+// UI Logic -------------------------------------------------------------------------------------------------------------------
+
+const printElements = (response, currency) => {
+    document.getElementById( "displayExchange" ).innerHTML = `The currency exchange from USD to ${currency} is: ${response.conversion_rates}`;
+};
+
+const printError = (error, currency) => {
+    document.getElementById("displayExchange").innerHTML = `There was an error accessing the currency exchange for ${currency}: ${error}`;
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const userCurrency = document.querySelector("input:checked.currencies").value;
+  const userAmount = document.getElementById("dollar").value;
+  getRates(userCurrency, userAmount);
+}; 
+
+
+window.addEventListener("load", function () {
+  document.getElementById("currencyExchanger").addEventListener("submit", handleSubmit);
+});
 
--- src/js/currencyAPI.js
+++ src/js/currencyAPI.js
@@ -1,9 +1,9 @@
 export default class CurrencyExchangerService {
-  static async getRates(dollar, currencies) {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencies}/${dollar}`); 
+  static async getRates(currency) {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
     const jsonifiedResponse = await response.json();
     if (!response.ok) {
-      const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse[error-type]}`;
+      const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
       throw new Error(errorMessage);
     }
     return jsonifiedResponse;
